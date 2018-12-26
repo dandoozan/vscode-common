b2fbcae237a4102736a3ed9aa17bf6c41c1aa865
Dan Doozan <dandoozan@gmail.com>
2018-12-25 20:46:56 -0700

    Add a lot of changes in utils

--- utils.ts
+++ utils.ts
@@ -4,7 +4,11 @@ import {
     window,
     TextEditor,
     Position,
+    Selection,
 } from 'vscode';
+import { parse, ParserOptions } from '@babel/parser';
+import { Node, isStringLiteral } from '@babel/types';
+import { isArray, isObject, isNumber } from 'lodash';
 
 export function addCommand(
     name: string,
@@ -24,8 +28,97 @@ export function getCurrentEditor() {
     return window.activeTextEditor;
 }
 
-export function getCursorPosition(editor: TextEditor) {
-    return editor.selection.active;
+export function getCursorLocation(editor: TextEditor) {
+    return editor.document.offsetAt(editor.selection.active);
+}
+
+export function getTextOfFile(editor: TextEditor) {
+    return editor.document.getText();
+}
+
+export function getLanguage(editor: TextEditor) {
+    return editor.document.languageId;
+}
+
+export function generateAst(code: string, language: string) {
+    //use try-catch b/c babel will throw an error if it can't parse the file
+    //(ie. if it runs into a "SyntaxError" or something that it can't handle)
+    //In this case, display a notification that an error occurred so that the
+    //user knows why the command didn't work
+    try {
+        const parserOptions: ParserOptions = {
+            sourceType: 'unambiguous', //auto-detect "script" files vs "module" files
+
+            //make the parser as lenient as possible
+            allowImportExportEverywhere: true,
+            allowAwaitOutsideFunction: true,
+            allowReturnOutsideFunction: true,
+            allowSuperOutsideMethod: true,
+        };
+
+        //add "typescript" plugin if language is typescript
+        if (language === 'typescript') {
+            parserOptions.plugins = ['typescript'];
+        }
+
+        return parse(code, parserOptions);
+    } catch (e) {
+        // console.log('​e=', e);
+        //do nothing, it will just return null below
+    }
+    return null;
+}
+
+function filterAst(astNode: Node, fnToApplyToEveryNode: Function) {
+    let filteredNodes: Node[] = [];
+
+    //if the current child is an array, just call filterAst on all
+    //it's elements
+    if (isArray(astNode)) {
+        //call filterAst on all children
+        for (const item of astNode) {
+            filteredNodes = filteredNodes.concat(
+                filterAst(item, fnToApplyToEveryNode)
+            );
+        }
+    } else if (isObject(astNode)) {
+        //apply the function to this node
+        if (fnToApplyToEveryNode(astNode)) {
+            //if it returns truthy, add this node to filteredNodes
+            filteredNodes.push(astNode);
+        }
+        //then call filterAst on all children
+        for (const key in astNode) {
+            if (astNode.hasOwnProperty(key)) {
+                filteredNodes = filteredNodes.concat(
+                    filterAst(astNode[key], fnToApplyToEveryNode)
+                );
+            }
+        }
+    }
+    return filteredNodes;
+}
+
+function isCursorInsideNode(cursorLocation: number, node: Node) {
+    return (
+        isNumber(node.start) &&
+        isNumber(node.end) &&
+        node.start <= cursorLocation &&
+        cursorLocation < node.end
+    );
+}
+
+export function findEnclosingString(ast: Node, cursorLocation: number) {
+    return filterAst(
+        ast,
+        (node: Node) =>
+            isStringLiteral(node) && isCursorInsideNode(cursorLocation, node)
+    );
+}
+
+export function setCursor(editor, offset) {
+    const pos = editor.document.positionAt(offset);
+    editor.selection = new Selection(pos, pos);
 }
 
 export function notify(msg: any) {
